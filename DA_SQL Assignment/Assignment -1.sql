d3a314f66eade37860489b1a999da23d298311fe
cobweb <cobwebmen@gmail.com>
2021-10-22 22:10:46 +0300

    commit

--- DA_SQL Assignment/Assignment -1.sql
+++ DA_SQL Assignment/Assignment -1.sql
@@ -27,16 +27,22 @@ group by Receiver_ID
 
 --Full (outer) join debits and credits tables on account id, taking net change as difference between credits and debits, coercing nulls to zeros with coalesce()
 
-SELECT	*
+ 
+SELECT	COALESCE(S.sender_id, R.receiver_id) AS Account_ID,
+		COALESCE(R.receive_amount, 0) - COALESCE(S.send_amount, 0) AS Net_Change
+
 FROM	(
 		SELECT	sender_id, SUM(amount) AS send_amount
 		FROM	assignment1
 		GROUP BY sender_id
 		) S
+
 FULL OUTER JOIN	
 		(
 		SELECT	receiver_id, SUM(amount) AS receive_amount
 		FROM	assignment1
 		GROUP BY receiver_id
 		) R
+
 ON		S.Sender_id = R.receiver_id
+;
